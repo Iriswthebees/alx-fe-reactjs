2fd403cf39eef3f3167b4a2d564b6c61d603fd52
IrisAbi <busybeeiris9@gmail.com>
2025-11-06 14:30:59 +0300

    Added UserProfile component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-
+import UserProfile from './components/UserProfile'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -16,6 +16,7 @@ function App() {
   return (
     <><WelcomeMessage />
       <Header /><MainContent /><Footer />
+      <UserProfile name="" age="" bio="" />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
