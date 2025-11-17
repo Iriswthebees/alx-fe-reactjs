22eb8700daa69e95148f3f593d84dac00c7f2fa8
IrisAbi <busybeeiris9@gmail.com>
2025-11-17 18:47:50 +0300

    Refractor prop drilling

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,10 +1,18 @@
-const UserProfile = (props) => {
-    return (
-        <div>
-            <h2> Name: {props.name} </h2>
-            <p> Age: {props.age} </p>
-            <p> Bio: {props.bio}</p>
-        </div>
-    );
-};
+import React, { useContext } from "react";
+import UserContext from "../UserContext";
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
+      <h2 style={{ color: "violet" }}>Name: {userData.name}</h2>
+      <p>
+        Age: <span style={{ fontWeight: "bold" }}>{userData.age}</span>
+      </p>
+      <p>Bio: {userData.bio}</p>
+    </div>
+  );
+}
+
 export default UserProfile;
