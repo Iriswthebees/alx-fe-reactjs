d94c3c26f3b173d0e377a39fddf9f1e2f443e7c4
IrisAbi <busybeeiris9@gmail.com>
2025-12-03 14:43:25 +0300

    Add advanced interactivity

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -9,17 +9,19 @@ export default function UserProfile() {
       text-center
     ">
       <img
-        src="https://via.placeholder.com/150"
+        src="/images/mrKento.jpg"
         alt="User"
         className="
           rounded-full 
           mx-auto 
           sm:w-24 sm:h-24 
           md:w-36 md:h-36
+          hover:scale-110 
+          transition-transform duration-300 ease-in-out
         "
       />
 
-      <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">
+      <h1 className="sm:text-lg md:text-xl hover:text-blue-800 my-4 transition-colors duration-300">
         John Doe
       </h1>
 
