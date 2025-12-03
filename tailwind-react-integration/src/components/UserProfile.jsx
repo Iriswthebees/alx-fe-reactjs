8e1659598f9d5bca08536f7cc61072a23c9fc12e
IrisAbi <busybeeiris9@gmail.com>
2025-12-03 13:35:22 +0300

    Add responsiveness on small/medium screens

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ export default function UserProfile() {
   return (
     <div className="
       bg-gray-100 
-      p-4 md:p-8 
+      sm:p-4 md:p-8 
       max-w-xs md:max-w-sm 
       mx-auto my-20 
       rounded-lg shadow-lg
@@ -14,16 +14,16 @@ export default function UserProfile() {
         className="
           rounded-full 
           mx-auto 
-          w-24 h-24 
+          sm:w-24 sm:h-24 
           md:w-36 md:h-36
         "
       />
 
-      <h1 className="text-lg md:text-xl text-blue-800 my-4">
+      <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">
         John Doe
       </h1>
 
-      <p className="text-sm md:text-base text-gray-600">
+      <p className="sm:text-sm md:text-base text-gray-600">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
