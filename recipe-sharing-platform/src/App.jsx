d8cc0c3e98196579df9b18155f89c6bc75ab607c
IrisAbi <busybeeiris9@gmail.com>
2025-12-09 20:18:10 +0300

    Install tailwindcss/postcss

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-blue-500 text-3xl font-bold">
+        Tailwind is Working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
