e7500dacf36cc96b2f4face7b6454636003fdbd8
IrisAbi <busybeeiris9@gmail.com>
2025-12-09 20:23:25 +0300

    Installed and setup tailwindcss/postcss

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+ module.exports = {
+  content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
